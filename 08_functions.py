dab304f3a28c234404f46bb305cf627b8286b1d8
ajaygnsharma <ajaygnsharma12@gmail.com>
2022-02-27 17:35:41 -0500

    Adding functions file

---
+++ 08_functions.py
@@ -0,0 +1,15 @@
+def simple_function():
+    print("This is a simple function");
+
+
+def country_info(country, capital):
+    print("This capital of %s is %s"%(country, capital));
+
+def add_this(a, b):
+    return a+b;
+
+        
+#simple_function();
+#country_info("India","Delhi");
+s = add_this(1,2);
+print(s);
